ad2802691d7a721a3d4b20059aea25e5f0ae59d0
simpson1177 <34554378+simpson1177@users.noreply.github.com>
2021-04-03 11:20:40 -0600

    Creating Models
    Create Methods for pizza, beer and wings

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,33 +1,40 @@
-import getUserId from '../utils/getUserId'
+import getUserId from "../utils/getUserId";
 
 const Query = {
-    users(parent, args, { prisma }, info) {
-        const opArgs = {
-            first: args.first,
-            skip: args.skip,
-            after: args.after,
-            orderBy: args.orderBy
-        }
-        
-        if (args.query) {
-            opArgs.where = {
-                OR: [{
-                    name_contains: args.query
-                }]
-            }
-        }
+  users(parent, args, { prisma }, info) {
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
+    };
 
-        return prisma.query.users(opArgs, info)
-    },
-    me(parent, args, { prisma, request }, info) {
-        const userId = getUserId(request)
-        
-        return prisma.query.user({
-            where: {
-                id: userId
-            }
-        })
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          {
+            name_contains: args.query,
+          },
+        ],
+      };
     }
-}
 
-export { Query as default }
+    return prisma.query.users(opArgs, info);
+  },
+  me(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    return prisma.query.user({
+      where: {
+        id: userId,
+      },
+    });
+  },
+  beers(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    return prisma.query.beers();
+  },
+};
+
+export { Query as default };
